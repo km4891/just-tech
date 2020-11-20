f6b3a07ac9de6023fde27dc50384f462ec6490bc
km4891 <kmorarity@gmail.com>
2020-11-19 17:29:53 -0800

    comments working

--- routes/api/post-route.js
+++ routes/api/post-route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 
@@ -7,21 +7,30 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-      // Query configuration
+      order: [['created_at', 'DESC']],
       attributes: [
-          'id',
-          'post_url',
-          'title',
-           'created_at'
-           [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
+        // include the Comment model here:
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
         }
       ]
-    })
+     })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
         console.log(err);
@@ -42,6 +51,14 @@ router.get('/', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
